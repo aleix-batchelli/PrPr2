76f9c5759a7fb08c76a7fa184a16a175e2337d1d
Marc Joan Sabater Riera <marcjoan.sabater@students.salle.url.edu>
2023-05-09 16:28:17 +0200

    add main logic and some more layouts

--- app/src/main/java/com/example/practica_final/MainActivity.java
+++ app/src/main/java/com/example/practica_final/MainActivity.java
@@ -1,15 +1,51 @@
 package com.example.practica_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private Button registerButton;
+    private EditText emailET;
+    private EditText passwordET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        setComponents();
+
+        registerButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create new activity for register view
+            }
+        }));
+
+        loginButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get data from email edit text and password edit text and check credentials
+
+                // create new activity for feed view
+            }
+        }));
+
+    }
+
+    private void setComponents() {
+        this.loginButton = findViewById(R.id.loginButton);
+        this.registerButton = findViewById(R.id.registerButton);
+        this.emailET = findViewById(R.id.emailEditText);
+        this.passwordET = findViewById(R.id.passwordEditText);
     }
 
 }
---
+++ app/src/main/java/com/example/practica_final/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.practica_final;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+
+    }
+
+}
