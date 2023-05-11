60063988de99af9f37983960ad16c778c3d9528d
Marc Joan Sabater Riera <marcjoan.sabater@students.salle.url.edu>
2023-05-11 14:50:31 +0200

    add main logic and some more layouts

--- app/src/main/java/com/example/practica_final/MainActivity.java
+++ app/src/main/java/com/example/practica_final/MainActivity.java
@@ -1,51 +1,51 @@
 package com.example.practica_final;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;
-    private Button registerButton;
-    private EditText emailET;
-    private EditText passwordET;
+    private ImageView logo;
+
+    private final int ID_LOGIN_ACTIVITY = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        setComponents();
+        setContentView(R.layout.activity_main);
 
-        registerButton.setOnClickListener((new View.OnClickListener() {
+        logo = (ImageView) findViewById(R.id.initialLogo);
+        logo.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // create new activity for register view
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivityForResult(intent,ID_LOGIN_ACTIVITY);
             }
         }));
 
-        loginButton.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // get data from email edit text and password edit text and check credentials
-
-                // create new activity for feed view
-            }
-        }));
 
     }
 
-    private void setComponents() {
-        this.loginButton = findViewById(R.id.loginButton);
-        this.registerButton = findViewById(R.id.registerButton);
-        this.emailET = findViewById(R.id.emailEditText);
-        this.passwordET = findViewById(R.id.passwordEditText);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode!= Activity.RESULT_OK){
+            return;
+        }
+
+        if(requestCode==ID_LOGIN_ACTIVITY){
+            return;
+        }
     }
 
 }
